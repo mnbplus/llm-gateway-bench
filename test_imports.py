7f25e53f2f79c504c2c3a22c7a3bfed8553fe355
mnbplus <hachiminamerutoa@gmail.com>
2026-03-15 17:00:51 +0800

    chore: gitignore coverage files

---
+++ test_imports.py
@@ -0,0 +1,8 @@
+import llm_gateway_bench.bench as b
+import llm_gateway_bench.formatters as f
+import llm_gateway_bench.history as h
+import llm_gateway_bench.validators as v
+print('bench PROVIDER_DEFAULTS:', len(b.PROVIDER_DEFAULTS))
+print('formatters ok')
+print('history ok')
+print('validators ok')
